77bf278d78b6234d7f50577534e23037c9458d7b
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-10-22 02:06:10 -0600

    Inicio GUI

---
+++ GUI/Proyecto2/src/main/java/com/mycompany/proyecto2/Proyecto2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2;
+
+/**
+ *
+ * @author sebastianqr.2208
+ */
+public class Proyecto2 {
+    static GUI gui;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        gui = new GUI();
+        gui.setVisible(true);
+    }
+}
